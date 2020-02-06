d6f0ccc6259bd5eb03691b4856d370212f3490e3
Jed Smit <jedsmit1@gmail.com>
2020-02-05 20:07:14 -0800

    deploy with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Connect Node to MySQL.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    port: 3306,
-    password: "rootpass",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        port: 3306,
+        password: "rootpass",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
